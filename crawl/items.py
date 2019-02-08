80cd2219c3f3db9c3c3123c5e6ab6d4949b0bef0
Hoang Le <>
2019-02-08 14:12:26 +0700

    first commit

---
+++ crawl/items.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from enum import Enum
+
+
+class TypeMovie(Enum):
+    oddMovies = 1
+    suiteMovies = 2
+
+
+class CrawlItem(scrapy.Item):
+    _id = scrapy.Field()
+    url_root = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    title_english = scrapy.Field()
+    thumbnail = scrapy.Field()
+    type = scrapy.Field()
+    kind = scrapy.Field()
+    duration = scrapy.Field()
+    status = scrapy.Field()
+    actors = scrapy.Field()
+    director = scrapy.Field()
+    release_year = scrapy.Field()
+    views = scrapy.Field()
---
+++ crawl/spiders/BomTanSpider.py
@@ -0,0 +1,60 @@
+import scrapy
+from crawl.items import CrawlItem, TypeMovie
+from scrapy_splash import SplashRequest
+
+class BomTanSpider(scrapy.Spider):
+    name = "bomtan"
+    allowed_domains = ['bomtan.net']
+    start_urls = ['http://bomtan.net/']
+
+    def start_requests_one_series_movie(self, response):
+        one_series_movie_url = response.xpath('//div[@id="menu-content"]/div/ul/'
+                                              'li[contains(h3/a/@title,"Phim lẻ")]/h3')
+        url = one_series_movie_url.xpath('.//a/@href').extract_first('').strip()
+        item = CrawlItem()
+        item['type'] = TypeMovie.oddMovies
+        request = SplashRequest(response.urljoin(url), endpoint="render.html", callback=self.parse_list)
+        request.meta['film'] = item
+        yield request
+
+    def start_requests_many_series_movie(self, response):
+        many_series_movie = '//div[@id="menu-content"]/div/ul/' \
+                            'li[contains(h3/a/@title,"Phim bộ")]/div/ul/li/h3'
+        for element in response.xpath(many_series_movie):
+            url = element.xpath('.//a/@href').extract_first('').strip()
+            item = CrawlItem()
+            item['type'] = TypeMovie.suiteMovies
+            request = SplashRequest(response.urljoin(url), endpoint="render.html", callback=self.parse_list)
+            request.meta['film'] = item
+            yield request
+
+    def parse_list(self, response):
+        film = response.meta['film'].copy()
+        for element in response.xpath('//div[@class="content"]/ul[@class="movies n_list"]/li/span[@class="name"]'):
+            url = element.xpath('.//a/@href').extract_first('').strip()
+            request = SplashRequest(response.urljoin(url), endpoint="render.html", callback=self.parse_detail)
+            request.meta['film'] = film
+            yield request
+
+    def parse_detail(self, response):
+        film = response.meta['film'].copy()
+        film['url_root'] = self.start_urls[0]
+        film["url"] = response.xpath('//p[@class="w_now"]/a[contains(text(),"xem phim")]/@href').get()
+        film["title"] = response.xpath('//div[@class="info_film"]/h1 ').get()
+        film["title_english"] = response.xpath('//div[@class="info_film"]/h2').get()
+        film["thumbnail"] = response.xpath('//span[@class="thumb-info"]/a/img/@src').get()
+        film["kind"] = response.xpath('//div[@class="info_film"]/ul/'
+                                      'li[contains(span/text(),"Thể loại")]/strong/a/@title').get()
+        film["duration"] = response.xpath('//div[@class="info_film"]/ul/'
+                                          'li[contains(span/text(),"Thời lượng")]/text()').get()
+        film["status"] = response.xpath('//div[@class="info_film"]/ul/'
+                                        'li[contains(span/text(),"Trạng thái")]/span/strong/text()').get()
+        film["actors"] = response.xpath('//div[@class="info_film"]/ul/'
+                                        'li[contains(span/text(),"Diễn viên")]/text()').get()
+        film["director"] = response.xpath('//div[@class="info_film"]/ul/'
+                                          'li[contains(span/text(),"Đạo diễn")]/text()').get()
+        film["release_year"] = response.xpath('//div[@class="info_film"]/ul/'
+                                              'li[contains(span/text(),"Năm phát hành")]/text()').get()
+        film["views"] = response.xpath('//div[@class="info_film"]/ul/'
+                                       'li[contains(span/text(),"Lượt xem")]/text()').get()
+        yield film
